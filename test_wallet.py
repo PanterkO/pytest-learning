4b699a4daaeb0eae6de09b84f2afb11afc6118f8
ADanilina <ladyintegral666@yandex.ru>
2017-09-14 15:56:33 +0300

    https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest Parametrized Test Functions

--- test_wallet.py
+++ test_wallet.py
@@ -35,3 +35,14 @@ def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet)
     #wallet = Wallet()
     with pytest.raises(InsufficientAmount):
         empty_wallet.spend_cash(100)
+
+@pytest.mark.parametrize("earned,spent,expected", [
+    (30, 10, 20),
+    (20, 2, 18),
+])
+
+def test_trasactions(earned, spent, expected):
+    my_wallet = Wallet()
+    my_wallet.add_cash(earned)
+    my_wallet.spend_cash(spent)
+    assert my_wallet.balance == expected
